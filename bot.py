ea5aadbafdd2eb8fbeae0d10e37654c452750555
MD-Developer-yt <animeworldday2005@gmail.com>
2026-05-28 12:22:59 +0530

    bot.py

---
+++ bot.py
@@ -0,0 +1,50 @@
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
+
+from threading import Thread
+from flask import Flask
+from pyrogram import Client
+import config
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Telegraph Bot Running"
+
+
+def run_web():
+    app.run(
+        host="0.0.0.0",
+        port=8080
+    )
+
+
+bot = Client(
+    "TelegraphUltraBot",
+    api_id=config.API_ID,
+    api_hash=config.API_HASH,
+    bot_token=config.BOT_TOKEN,
+    plugins=dict(root="plugins")
+)
+
+Thread(target=run_web).start()
+
+print("""
+╔══════════════════════╗
+║   ᴍᴅ-ᴅᴇᴠᴇʟᴏᴘᴇʀ-ʏᴛ         ║
+║   ᴛᴇʟᴇɢʀᴀᴘʜ ꜱᴛᴀʀᴛᴇᴅ.       ║
+╚══════════════════════╝
+""")
+
+bot.run()
+
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
