6e9f4a6f977cd67abc5d0deae1f4aeb9031ea44d
MD-Developer-yt <animeworldday2005@gmail.com>
2026-05-28 12:28:44 +0530

    config.py

---
+++ config.py
@@ -0,0 +1,29 @@
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
+
+import os
+
+API_ID = int(os.environ.get("API_ID", "123456"))
+API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
+
+OWNER_ID = int(os.environ.get("OWNER_ID", "7284759394"))
+
+MONGO_URL = os.environ.get("MONGO_URL", "")
+
+DB_NAME = "telegraph_ultra"
+
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
