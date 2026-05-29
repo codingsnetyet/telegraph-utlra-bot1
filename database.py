588d2ad1f5bd08624e289984aa22c793f55a98aa
codingsnetyet <codingsnetyet@gmail.com>
2026-05-29 20:36:56 +0530

    database.py

--- database.py
+++ database.py
@@ -8,36 +8,25 @@ from pymongo import MongoClient
 import config
 import time
 
-user_images = db.user_images
-
 # ---------------- CONNECTION ---------------- #
 
 client = MongoClient(config.MONGO_URL, serverSelectionTimeoutMS=5000)
 
 db = client[config.DB_NAME]
 
+# ---------------- COLLECTIONS ---------------- #
+
 users = db.users
 posts = db.posts
 batches = db.batches
+user_images = db.user_images
 
-# ------------------------- #
-# Don't Remove Credit 
-# Ask Doubt @AU_Bot_Discussion 
-# Owner @Mr_Mohammed_29 
-# ------------------------- #
-
-# ---------------- INDEXES (IMPORTANT) ---------------- #
+# ---------------- INDEXES ---------------- #
 
 users.create_index("user_id", unique=True)
 posts.create_index("user_id")
 batches.create_index("user_id")
-
-# ------------------------- #
-# Don't Remove Credit 
-# Ask Doubt @AU_Bot_Discussion 
-# Owner @Mr_Mohammed_29 
-# ------------------------- #
-
+user_images.create_index("user_id")
 
 # ---------------- USER ---------------- #
 
@@ -54,13 +43,6 @@ def add_user(user_id):
         upsert=True
     )
 
-# ------------------------- #
-# Don't Remove Credit 
-# Ask Doubt @AU_Bot_Discussion 
-# Owner @Mr_Mohammed_29 
-# ------------------------- #
-
-
 # ---------------- POSTS ---------------- #
 
 def save_post(user_id, url, title):
@@ -70,12 +52,6 @@ def save_post(user_id, url, title):
         "title": title,
         "time": int(time.time())
     })
-    
-# ------------------------- #
-# Don't Remove Credit 
-# Ask Doubt @AU_Bot_Discussion 
-# Owner @Mr_Mohammed_29 
-# ------------------------- #
 
 # ---------------- BATCH ---------------- #
 
@@ -86,21 +62,13 @@ def save_batch(user_id, urls):
         "time": int(time.time())
     })
 
-# ------------------------- #
-# Don't Remove Credit 
-# Ask Doubt @AU_Bot_Discussion 
-# Owner @Mr_Mohammed_29 
-# ------------------------- #
-
 # ---------------- STATS ---------------- #
 
 def total_users():
     return users.count_documents({})
 
-
 def total_posts():
     return posts.count_documents({})
-
 
 def total_batches():
     return batches.count_documents({})
@@ -110,3 +78,72 @@ def total_batches():
 # Ask Doubt @AU_Bot_Discussion 
 # Owner @Mr_Mohammed_29 
 # ------------------------- #
+
+from pymongo import MongoClient
+import config
+import time
+
+# ---------------- CONNECTION ---------------- #
+
+client = MongoClient(config.MONGO_URL, serverSelectionTimeoutMS=5000)
+
+db = client[config.DB_NAME]
+
+# ---------------- COLLECTIONS ---------------- #
+
+users = db.users
+posts = db.posts
+batches = db.batches
+user_images = db.user_images
+
+# ---------------- INDEXES ---------------- #
+
+users.create_index("user_id", unique=True)
+posts.create_index("user_id")
+batches.create_index("user_id")
+user_images.create_index("user_id")
+
+# ---------------- USER ---------------- #
+
+def add_user(user_id):
+    if not user_id:
+        return
+
+    users.update_one(
+        {"user_id": user_id},
+        {"$set": {
+            "user_id": user_id,
+            "time": int(time.time())
+        }},
+        upsert=True
+    )
+
+# ---------------- POSTS ---------------- #
+
+def save_post(user_id, url, title):
+    posts.insert_one({
+        "user_id": user_id,
+        "url": url,
+        "title": title,
+        "time": int(time.time())
+    })
+
+# ---------------- BATCH ---------------- #
+
+def save_batch(user_id, urls):
+    batches.insert_one({
+        "user_id": user_id,
+        "urls": urls,
+        "time": int(time.time())
+    })
+
+# ---------------- STATS ---------------- #
+
+def total_users():
+    return users.count_documents({})
+
+def total_posts():
+    return posts.count_documents({})
+
+def total_batches():
+    return batches.count_documents({})
