0ae8999460d50e771880973c37a6b5d5dc21208e
codingsnetyet <codingsnetyet@gmail.com>
2026-05-29 16:40:15 +0530

    database.py

--- database.py
+++ database.py
@@ -6,8 +6,11 @@
 
 from pymongo import MongoClient
 import config
+import time
 
-client = MongoClient(config.MONGO_URL)
+# ---------------- CONNECTION ---------------- #
+
+client = MongoClient(config.MONGO_URL, serverSelectionTimeoutMS=5000)
 
 db = client[config.DB_NAME]
 
@@ -21,10 +24,31 @@ batches = db.batches
 # Owner @Mr_Mohammed_29 
 # ------------------------- #
 
+# ---------------- INDEXES (IMPORTANT) ---------------- #
+
+users.create_index("user_id", unique=True)
+posts.create_index("user_id")
+batches.create_index("user_id")
+
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
+
+
+# ---------------- USER ---------------- #
+
 def add_user(user_id):
+    if not user_id:
+        return
+
     users.update_one(
         {"user_id": user_id},
-        {"$set": {"user_id": user_id}},
+        {"$set": {
+            "user_id": user_id,
+            "time": int(time.time())
+        }},
         upsert=True
     )
 
@@ -34,23 +58,30 @@ def add_user(user_id):
 # Owner @Mr_Mohammed_29 
 # ------------------------- #
 
+
+# ---------------- POSTS ---------------- #
+
 def save_post(user_id, url, title):
     posts.insert_one({
         "user_id": user_id,
         "url": url,
-        "title": title
+        "title": title,
+        "time": int(time.time())
     })
-
+    
 # ------------------------- #
 # Don't Remove Credit 
 # Ask Doubt @AU_Bot_Discussion 
 # Owner @Mr_Mohammed_29 
 # ------------------------- #
+
+# ---------------- BATCH ---------------- #
 
 def save_batch(user_id, urls):
     batches.insert_one({
         "user_id": user_id,
-        "urls": urls
+        "urls": urls,
+        "time": int(time.time())
     })
 
 # ------------------------- #
@@ -59,17 +90,18 @@ def save_batch(user_id, urls):
 # Owner @Mr_Mohammed_29 
 # ------------------------- #
 
+# ---------------- STATS ---------------- #
+
 def total_users():
     return users.count_documents({})
 
-# ------------------------- #
-# Don't Remove Credit 
-# Ask Doubt @AU_Bot_Discussion 
-# Owner @Mr_Mohammed_29 
-# ------------------------- #
 
 def total_posts():
     return posts.count_documents({})
+
+
+def total_batches():
+    return batches.count_documents({})
 
 # ------------------------- #
 # Don't Remove Credit 
